6588532c2ba1beec07b9813070b06c3610b5dc7d
RitaChen0808 <71751332+RitaChen0808@users.noreply.github.com>
2020-10-14 15:43:21 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    for (let Y = 0; Y <= 4; Y++) {
+        if (Y == 1) {
+            continue;
+        }
+        for (let X = 0; X <= 4; X++) {
+            led.plot(X, Y)
+            basic.pause(200)
+        }
+    }
+    basic.clearScreen()
+})
